9e3c3820fa27f64ef6bb0967f756f0fca7749133
simone <simone.giacomelli@gmail.com>
2025-06-04 10:04:15 +0100

    Add src attribute to Link dataclass in compute_links function

--- email-import/common/fix_img.py
+++ email-import/common/fix_img.py
@@ -2,30 +2,32 @@ from dataclasses import dataclass, field
 import re
 from collections import defaultdict
 
+
 @dataclass
 class Link:
     new_alt: str
-    src: str = ''
+    src: str
 
 
 def compute_links(html, resource_prefix) -> list[Link]:
-    # Find all <img ... alt="..."> tags
-    img_tags = re.findall(r'<img [^>]*alt="([^"]+)"[^>]*>', html)
+    # Find all <img ... alt="..." ... src="..."> tags
+    img_tags = re.findall(r'<img [^>]*alt="([^"]+)"[^>]*src="([^"]+)"[^>]*>', html)
     alt_counts = defaultdict(int)
     alt_indices = defaultdict(int)
     # Count occurrences of each alt
-    for alt in img_tags:
+    for alt, _ in img_tags:
         alt_counts[alt] += 1
     result = []
-    for alt in img_tags:
+    for alt, src in img_tags:
         if alt_counts[alt] > 1:
             idx = alt_indices[alt]
             new_alt = f"{resource_prefix}{alt.replace('.', f'-{idx:02}.')}"
             alt_indices[alt] += 1
         else:
             new_alt = f"{resource_prefix}{alt}"
-        result.append(Link(new_alt))
+        result.append(Link(new_alt, src))
     return result
+
 
 @dataclass
 class FixImg:
